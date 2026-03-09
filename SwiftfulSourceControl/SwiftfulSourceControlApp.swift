9a8d2c4c1b5478fccf98ce6852aa7f5fe11a5242
Tarik <otariksener7@outlook.com>
2026-03-09 17:03:11 +0300

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Ömer Tarık Şener on 9.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftfulSourceControlTests/SwiftfulSourceControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlTests.swift
+//  SwiftfulSourceControlTests
+//
+//  Created by Ömer Tarık Şener on 9.03.2026.
+//
+
+import Testing
+@testable import SwiftfulSourceControl
+
+struct SwiftfulSourceControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
