323312bc1f5b58097d7beffa12692dcb1cb579dd
Tarik <otariksener7@outlook.com>
2026-03-10 13:42:11 +0300

    Add click me button

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -15,7 +15,10 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Tarik Tarik Tarik !")
-            
+        
+            Button("Click me"){
+                
+            }
         
         }
         .padding()
