021470f386333925816d8088c99fa36dbc6f6c5d
Tarik <otariksener7@outlook.com>
2026-03-10 15:50:42 +0300

    [Feature] Update Subscribe text to Subscribe Now ! (was Subscribe)

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -36,7 +36,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Tarik !")
             
-            Button("Subscribe"){
+            Button("Subscribe now !"){
                 
             }
         
