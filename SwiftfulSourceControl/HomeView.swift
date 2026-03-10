075ed894241507fc9b043b7ebbd025939a73d3a7
Tarik <otariksener7@outlook.com>
2026-03-10 14:08:18 +0300

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Ömer Tarık Şener on 10.03.2026.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
